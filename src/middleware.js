660e2fd59b956c7e9e9f4f47016226ac6176a093
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-27 20:57:08 +0600

    practice middleware

---
+++ src/middleware.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextResponse } from 'next/server';
+
+export const middleware = ( request ) => {
+        // if(request.nextUrl.pathname !='/login'){
+          return  NextResponse.redirect(new URL('/login', request.url))
+        // }
+};
+
+export const config = {
+    matcher : '/about/:path*'
+}
