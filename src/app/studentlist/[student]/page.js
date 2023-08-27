970ab079814cfcbe81d0d428234aa1587cc09dc6
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-27 20:13:11 +0600

    dynamic route

---
+++ src/app/studentlist/[student]/page.js
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+
+const Student = ({params}) => {
+    return (
+        <div>
+            <h1>Student Details</h1>
+            <h3>Name : {params.student}</h3>           
+        </div>
+    );
+};
+
+export default Student;
