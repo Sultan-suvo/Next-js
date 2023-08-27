11d9bda9dc3702c5a55872d551bede1d33fb8ae8
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-27 20:31:46 +0600

    create 404 page

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutNotFound = () => {
+    return (
+        <div>
+            <h1>This about page is not avilable</h1>
+        </div>
+    );
+};
+
+export default AboutNotFound;
