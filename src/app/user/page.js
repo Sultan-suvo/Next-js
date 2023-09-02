63daf8984d3123ffd16be5c508fef348af4fbd7c
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-09-02 15:34:56 +0600

    dynamic title in user page

---
+++ src/app/user/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Page() {
+    return (
+        <div>
+            user page
+        </div>
+    );
+};
+
+export function generateMetadata() {
+    return {
+        title: 'user page',
+        description:'add'
+        
+    }
+}
