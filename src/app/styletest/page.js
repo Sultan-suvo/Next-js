ccd6c1873336c2f31ca0d5b3303f08e3ab414e19
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-09-02 15:00:16 +0600

    conditional style

--- src/app/styletest/page.js
+++ src/app/styletest/page.js
@@ -7,6 +7,8 @@ import outside from '@/app/style/outside.module.css'
 
 export default function Page() {
     const [h3Style, setH3Style] = useState({ backgroundColor: 'green' })
+    const [color, setColor] = useState('red')
+    const {blue} = outside
     return (
         <div>
             <h1>Styletest</h1>
@@ -18,6 +20,13 @@ export default function Page() {
             <h3 className={custom.main}>module css</h3>
             <h3 className={other.main}>module css</h3>
             <h3 className={outside.main}>module css</h3>
+
+            {/* another vdo */}
+            <h3 className={color == 'red' ? outside.red : outside.green}>conditional css</h3>
+            <h2 style={{backgroundColor:color=='red' ? 'red': 'green'}}>Heading2</h2>
+            <h3 id={outside.blue}>Heading3</h3>
+            <h3 id={blue}>Heading4</h3>
+            <button onClick={() => setColor('green')}>Update Style</button>
         </div>
     );
 };
