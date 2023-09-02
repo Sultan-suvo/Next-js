6107cf5015e9f59e4b66716c07e168dda744e7c0
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-09-02 15:56:16 +0600

    loading feature

---
+++ src/app/users/page.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+async function fetchUsers() {
+    const response = await fetch('https://dummyjson.com/users');
+    const data = await response.json();
+    return data.users;
+}
+export default async function Page() {
+    const users = await fetchUsers();
+    console.log(users);
+
+    return (
+        <div>
+            {
+                users.map((user) => (
+                    <h3 key={user.id}>FirstName: {user.firstName}</h3>
+                ))
+            }
+        </div>
+    );
+}
